cc5fc769436674a1489b479304e92687a3ff6eda
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-01-13 13:03:53 +0100

    subida conexión mongo

---
+++ controllers/questionsApi.js
@@ -0,0 +1,17 @@
+const Question = require('../models/question');
+
+const getAllQuestions = async(req, res) => {
+    let data;
+    try {
+        data = await Question.find({}, '-_id');
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json({"error":error});
+    }
+};
+
+getQuestions = {
+    getAllQuestions
+}
+
+module.exports = getQuestions;
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 5000;
+const questionsApi = require('./controllers/questionsApi')
+const cors = require('cors');
+
+require('./utils/dbmongo');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).send('Plantilla back')
 })
+
+app.get('/api/questions', questionsApi.getAllQuestions);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)

65ff365d9e00ebc7ec174c6a06a2b2a57803b3e2
Taniuop <taniuop@gmail.com>
2022-01-16 17:11:13 +0100

    pre merge

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 5000;
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+
 require('./utils/dbmongocon');
-const bodyParser = require('body-parser');
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Port 
+const port = 5000;
+
 // para traernos datos de objetos de un formulario,permite procesarlo y crear el objeto.Extended, no acepta datos como imagen
 app.use(bodyParser.json({ limit: "20mb", extended: true })) //we are going to be sendig some images that have to be limited in size
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }))
+
+// Routes 
 app.use(require('./routes/routesUser'));
 
-//Routes
-app.get('/', (req, res) => {
-  res.status(200).send('Plantilla back')
-})
 
 //Conexion server
 app.listen(port, () => {

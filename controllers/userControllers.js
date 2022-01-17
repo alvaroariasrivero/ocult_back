4f7f1e4d958da2efc73ffea57c9cae3c8e461aa9
IsaGuapo <isabela.guapo@gmail.com>
2022-01-17 16:41:28 +0100

    change userControllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -20,63 +20,59 @@ const getUsers = async (res,req) => {
     return response
 }
 
-//Funcion para crear usuario-Registrar
-const createUser = async (res,req) => {
-    try{
-        const {id_company, name, email, password, image} = req.body;
-        let errors = [];
-
-        if(!id_company || !name || !email || !password ) {
-            errors.push({msg : "Completa todos los campos"})
-        }
-
-        if(password.length <= 6) {
-            errors.push({msg : "La contraseña tiene que tener al menos 6 dígitos"});
-            
-        }  else {
-            const result = await User.createUser(req.body);
-
-                if (result === 1) {
-                    console.log("usuario creado")
-                } else {
-                    console.log("error")
-                }
-        }
-    } catch (error) {
-        console.log(error)
-    }    
-};
-
-
-
-
-
-
+// //Funcion para crear usuario-Registrar
 // const createUser = async (res,req) => {
-//     // let userData = req.body;
-//     console.log(req.userData)
-//     const {id_company, name, email, password, image} = req.body;
-//     let connection,response;
-//     try {
-//         connection = await pool.connect();
-//         // response = await pool.query(`INSERT INTO users(id_company, name, email,password,image) VALUES ($1, $2, $3, $4, $5 )`,
-//         // [
-//         //     id_company, 
-//         //     name, 
-//         //     email, 
-//         //     password,
-//         //     image
-//         // ]
-//         // );
-//         // res.status(200).json(response.rows)
-//     } catch (error) {
-//         console.log(error);
+//     try{
+//         const {id_company, name, email, password, image} = req.body;
+//         let errors = [];
 
-//     } finally {
-//         connection.release();
-//     }
-//      return response
-// }
+//         if(!id_company || !name || !email || !password ) {
+//             errors.push({msg : "Completa todos los campos"})
+//         }
+
+//         if(password.length <= 6) {
+//             errors.push({msg : "La contraseña tiene que tener al menos 6 dígitos"});
+
+//         }  else {
+//             const result = await User.createUser(req.body);
+
+//                 if (result === 1) {
+//                     console.log("usuario creado")
+//                 } else {
+//                     console.log("error")
+//                 }
+//         }
+//     } catch (error) {
+//         console.log(error)
+//     }    
+// };
+
+
+const createUser = async (res,req) => {
+    // let userData = req.body;
+    console.log(req.userData)
+    const {id_company, name, email, password, image} = req.body;
+    let connection,response;
+    try {
+        connection = await pool.connect();
+        response = await pool.query(`INSERT INTO users(id_company, name, email,password,image) VALUES ($1, $2, $3, $4, $5 )`,
+        [
+            id_company, 
+            name, 
+            email, 
+            password,
+            image
+        ]
+        );
+        res.status(200).json(response.rows)
+    } catch (error) {
+        console.log(error);
+
+    } finally {
+        connection.release();
+    }
+     return response
+}
 
 
 module.exports = {

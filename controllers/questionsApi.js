adf68cae7ed19deb5813626d16ee1d5fe9453690
Taniuop <taniuop@gmail.com>
2022-01-20 16:45:41 +0100

    MERGE x 2

--- controllers/questionsApi.js
+++ controllers/questionsApi.js
@@ -4,7 +4,8 @@ const getAllQuestions = async(req, res) => {
     let data;
     try {
         alldata = await Question.find({}, '-_id');
-        data = alldata.slice(0, 4)
+        data = alldata
+        // .slice(0, 4)
         res.status(200).json(data);
     } catch (error) {
         res.status(400).json({"error":error});
